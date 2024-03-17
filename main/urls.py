4f81e74ac13a3ceb97337e7ab13237713a7f418e
karpov vladislav andreevich <parulit2000@mail.ru>
2024-03-18 01:00:27 +0300

    Commit

--- main/urls.py
+++ main/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('reg', RegisterUser.as_view(), name='register'),
 
 
-    path('events', eventfunc, name='events' ),
-    path('Eventsinfo', enentinfofunc, name='eventsinfo' ),
-    path('profile', profilefunc, name='profile' ),
+    path('Events', eventfunc, name='events'),
+    path('Eventsinfo', enentinfofunc, name='eventsinfo'),
+    path('profile', profilefunc, name='profile'),
 
 
 ]
