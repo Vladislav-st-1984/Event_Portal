1856df5694377348a88e0c80dd65692017620067
Nekeey01 <78684029+Nekeey01@users.noreply.github.com>
2024-04-04 00:16:36 +0300

    abeb

--- main/views.py
+++ main/views.py
@@ -180,6 +180,7 @@ def enentinfofunc(request):
 
 class EventDetail(DataMixin, ListView):
     template_name = "main/Eventsinfo.html"
+    context_object_name = "Event"
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
